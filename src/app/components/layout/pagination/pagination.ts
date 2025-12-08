58676f00a4121a4f57e8f8c6491240be2e63191d
Fabian <brandt.fabian@gmail.com>
2025-12-08 16:48:57 +0100

    add translate to pagination

--- src/app/components/layout/pagination/pagination.ts
+++ src/app/components/layout/pagination/pagination.ts
@@ -1,8 +1,9 @@
 import { Component, computed, input, model, output } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-pagination',
-    imports: [],
+    imports: [TranslatePipe],
     templateUrl: './pagination.html',
     styleUrl: './pagination.scss',
 })
