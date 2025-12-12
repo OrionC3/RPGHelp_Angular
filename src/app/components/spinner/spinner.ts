1899ff41eaaa6c47c6d414a0d43759facf438536
OrionC3 <93842749+OrionC3@users.noreply.github.com>
2025-12-12 10:41:06 +0100

    spinner translation

--- src/app/components/spinner/spinner.ts
+++ src/app/components/spinner/spinner.ts
@@ -1,11 +1,12 @@
 import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { LoadingService } from '@core/services/loading-service';
+import { TranslatePipe } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-spinner',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, TranslatePipe],
   templateUrl: './spinner.html',
   styleUrl: './spinner.scss',
 })
